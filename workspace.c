bb1b1cee9d0bf938fdc35624e050629a49033931
hafideveloper <hafideveloperr@gmail.com>
2024-11-14 16:04:25 +0300

    Metric unit conversions

--- workspace.c
+++ workspace.c
@@ -1,16 +1,19 @@
-// assigning values to a variable in different number systems
+// cm, m and km unit conversions
 
 #include <stdio.h>
 
 int main(){
 
-    int a = 35;
-    int b = 0b00100011;
-    int c = 0x23;
+    float cm, m, km;
 
-    printf("a = %d\n", a); // In the number order of 10 (decimal)
-    printf("b = %d\n", b); // In the number order of 2 (binary)
-    printf("c = %d\n", c); // In the number order of 16 (hexadecimal)
+    printf("Enter the length in centimetres: ");
+    scanf("%f", &cm);
+
+    m = cm / 100.0;
+    km = cm / 100000.0;
+    
+    printf("Length in metres = %.2f m \n", m);
+    printf("Length in kilometres = %.2f km", km);
 
     return 0;
 }
