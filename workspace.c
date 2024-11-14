1729d357a4fb31e9ddf048bce502bb74283a1150
hafideveloper <hafideveloperr@gmail.com>
2024-11-14 17:32:09 +0300

    compound interest calculation

--- workspace.c
+++ workspace.c
@@ -1,10 +1,11 @@
-// Simple interest calculation
+// Simple and compound interest calculation
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
-    float principal, time, interestRate, interestAmount;
+    float principal, time, interest_rate, simple_interest_amount, compound_interest_amount;
 
     printf("Enter principal amount: ");
     scanf("%f", &principal);
@@ -13,11 +14,15 @@ int main(){
     scanf("%f", &time);
 
     printf("Enter interest rate: ");
-    scanf("%f", &interestRate);
+    scanf("%f", &interest_rate);
 
-    interestAmount = (principal * time * interestRate) / 100;
+    simple_interest_amount = (principal * time * interest_rate) / 100; //simple interest calculation 
 
-    printf("The amount of interest calculated by simple interest calculation = %f", interestAmount);
+    printf("Interest calculated by simple interest calculation = %f", simple_interest_amount );
+
+    compound_interest_amount = principal * (pow((1 + interest_rate / 100), time)); // compound interest calculation
+
+    printf("Interest calculated by compound interest calculation = %f", compound_interest_amount);
 
     return 0;
 }
