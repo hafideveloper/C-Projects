2866bdc984a4a6c3e2a86b7a9fc827d31ea0ab66
hafideveloper <hafideveloperr@gmail.com>
2024-11-18 23:48:32 +0300

    difference between strlen and sizeof

--- workspace.c
+++ workspace.c
@@ -1,15 +1,25 @@
-// operations with strings 
+// difference between strlen and sizeof
 
 #include <stdio.h>
 
 int main(){
 
-    char  welcome_message1[]= "Hello!"; // contains null character (\0) 
-    char  welcome_message2[]= {'H', 'e', 'l', 'l', 'o', '!', '\0'}; // (\0 -> string terminator)
-
-    printf("%lu\n", sizeof(welcome_message1));
+    char english_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%d", strlen(english_alphabet));
     printf("\n...\n");
-    printf("%lu\n", sizeof(welcome_message2));
+    printf("%d", sizeof(english_alphabet)); //sizeof includes the string terminator
+    printf("\n............\n");
 
+    char turkish_alphabet[] = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ"; //Special characters in Turkish (e.g. Ç, Ğ, İ, Ö, Ş, Ü) usually occupy 2 bytes in UTF-8 encoding
+    printf("%d", strlen(turkish_alphabet));
+    printf("\n...\n");
+    printf("%d", sizeof(turkish_alphabet));
+    printf("\n............\n");
+
+    char trial[50] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%d", strlen(trial));
+    printf("\n...\n");
+    printf("%d", sizeof(trial)); //Measures the total memory allocated when the array is defined
+ 
     return 0;
 }
