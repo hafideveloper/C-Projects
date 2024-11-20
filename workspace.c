dce5d5d66937f4cb64a8665665a8c94841734bd2
hafideveloper <hafideveloperr@gmail.com>
2024-11-20 23:50:12 +0300

    Employee struct: init, print, copy

--- workspace.c
+++ workspace.c
@@ -1,30 +1,54 @@
-// string concatenation, copying, and comparison.
+// struct
 
 #include <stdio.h>
 #include <string.h>
 
+// Define a structure for employee information
+struct EmployeeInfo
+{
+    int Age;
+    float Salary;
+    char Name[30];
+    char Gender[8];
+};
+
 int main(){
+    // Initialize first employee data
+    struct EmployeeInfo Employee1;
+    strcpy(Employee1.Name, "Ahmet");
+    strcpy(Employee1.Gender, "Male");
+    Employee1.Age = 34;
+    Employee1.Salary = 8500;
 
-    char greeting_message[20] = "Hello ";
-    char additional_text[] = "World!";
-    char copied_message[20];
+    // Print first employee details
+    printf("Employee Name: %s\n", Employee1.Name);
+    printf("Employee Gender: %s\n", Employee1.Gender);
+    printf("Employee Age: %d\n", Employee1.Age);
+    printf("Employee Salary: %lf\n", Employee1.Salary);
 
-    // Concatenating strings
-    strcat(greeting_message, additional_text); // it appends "World!" to the end of "Hello " (end-to-end addition)
-    printf("%s", greeting_message);
-    printf("\n...\n");
+    printf("\n..........\n");
 
-    // Copying string
-    strcpy(copied_message, greeting_message); // it copies greeting_message into copied_message
-    printf("%s", copied_message);
-    printf("\n...\n");
+    // Initialize second employee data using direct initialization
+    struct EmployeeInfo Employee2 = {33, 8800, "Ela", "Female"};
 
-    // Comparing strings
-    printf("%d\n", strcmp(greeting_message, copied_message)); // the strcmp function compares three strings. It returns 0 if they are equal, or a positive/negative value if they are different
-    printf("\n...\n");
-    printf("%d\n", strcmp(greeting_message, additional_text));
-    printf("\n...\n");
-    printf("%d\n", strcmp(additional_text, greeting_message));
+    // Print first employee details
+    printf("Employee Name: %s\n", Employee2.Name);
+    printf("Employee Gender: %s\n", Employee2.Gender);
+    printf("Employee Age: %d\n", Employee2.Age);
+    printf("Employee Salary: %lf\n", Employee2.Salary);
+
+    printf("\n..........\n");
+
+    // Copy second employee data into a new structure
+    struct EmployeeInfo Employee2Copy;
+    Employee2Copy = Employee2;
+
+    // Print copied employee details
+    printf("Employee Name: %s\n", Employee2Copy.Name);
+    printf("Employee Gender: %s\n", Employee2Copy.Gender);
+    printf("Employee Age: %d\n", Employee2Copy.Age);
+    printf("Employee Salary: %lf\n", Employee2Copy.Salary);
+
 
     return 0;
 }
