9f18837292141e4b623b0818b813b15085cc1d61
hafideveloper <hafideveloperr@gmail.com>
2024-11-21 02:02:58 +0300

    address-of operator

--- workspace.c
+++ workspace.c
@@ -1,21 +1,19 @@
 
-// enumerated type
+// access to variable addresses
 
 #include <stdio.h>
-#include <string.h>
-enum Levels
-{
-    LOW,
-    MEDIUM,
-    HIGH
-};
 
 int main(){
 
-    enum Levels RoomTemperature = LOW;
+    char character = 'C';
+    int integer = 1;
+    float real_number = 10.4f;
+    long long large_integer = 98989898911;
 
-    // Print the integer value of RoomTemperature (which will be 0 since LOW is the first value)
-    printf("%d", RoomTemperature);
+    printf("value of character variable = %c, address of character variable = %u\n", character, &character);
+    printf("value of integer variable = %c, address of integer variable = %u\n", integer, &integer);
+    printf("value of real_number variable = %c, address of real_number variable = %u\n", real_number, &real_number);
+    printf("value of large_integer variable = %c, address of large_integer variable = %u\n", large_integer, &large_integer);
 
     return 0;
 }
