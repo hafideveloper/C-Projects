47e35ba2e93f217689f6c04b4e072e16baccd10a
hafideveloper <hafideveloperr@gmail.com>
2024-12-02 01:05:21 +0300

    print numbers with for loop

--- workspace.c
+++ workspace.c
@@ -1,47 +1,26 @@
 
-// Program to calculate the roots of a quadratic equation using switch-case
-// Includes handling for real and distinct roots, complex roots, and equal roots
+// Printing counting numbers up to a given number with a for loop
 
 #include <stdio.h>
-#include <math.h>
 
 int main(){
 
-    float a, b, c;
-    float root1, root2, imaginary;
-    float discriminant;
+    int i, n;
 
-    printf("Enter the coefficients a, b, and c of the quadratic equation in the form (ax^2 + bx + c): ");
-    scanf("%f%f%f", &a, &b, &c);
+    printf("Enter the upper limit value: ");
+    scanf("%d", &n);
 
-    // Calculate the discriminant to determine the nature of the roots.
-    discriminant = (b * b) - (4 * a * c);
+    printf("All counting numbers from 1 to %d: \n", n);
 
-    switch (discriminant > 0)
+    /* Loop from 1 to the given number 'n'
+       'i' is initialized to 1, the loop continues while 'i' is less than or equal to 'n', 
+       and 'i' is incremented by 1 in each iteration
+    */
+
+    for(i = 1; i<=n; i++)
     {
-    case 1: // Case when discriminant > 0: Two distinct and real roots
-        root1 = (-b + sqrt(discriminant)) / (2 * a); 
-        root2 = (-b - sqrt(discriminant)) / (2 * a);        
-        printf("There are two distinct and real roots: %.2f and %.2f", root1, root2);      
-        break;
-    
-    case 0: // Case when discriminant <= 0: Need further checks for exact scenario
-        switch (discriminant < 0)
-        {
-            case 1: 
-            // Case when discriminant < 0: Two complex (imaginary) roots.
-            root1 = root2 = -b / (2 * a);
-            imaginary = sqrt(-discriminant) / (2 * a);
-            printf("There are two distinct and complex roots: %.2f + i%.2f and %.2f - i%.2f", root1, imaginary, root2, imaginary);
-            break;
-
-            case 0:
-            // Case when discriminant == 0: Two real and equal roots.
-            root1 = root2 = -b / (2 * a);
-            printf("There are two real roots equal to each other: %.2f and %.2f", root1, root2);
-            break;
-
-        }
+        printf("%d\n", i);
     }
+    
     return 0;
 }
