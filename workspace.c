5db6325ac855656168a4ea973edc5b8f62ee7d2e
hafideveloper <hafideveloperr@gmail.com>
2025-01-06 13:47:02 +0300

    Using break in a for loop

--- workspace.c
+++ workspace.c
@@ -1,20 +1,18 @@
 
-// Creating the multiplication table of a given number
+// How the break command works (in a for loop)
 
 #include <stdio.h>
 
 int main(){
 
-    int i, number;
+    int i = 0;
 
-    // Receiving the number from the user for which the multiplication table will be created
-    printf("Enter the number for which the multiplication table is to be created: ");
-    scanf("%d", &number);
-
-    //Printing the multiplication table from 1 to 10 for the given number
     for(i=1; i<=10; i++)
     {
-        printf("%d * %d = %d\n", number, i, (number * i));
+        if (i == 4) {
+            break;
+        }
+        printf("%d\n", i);
     }
        
     return 0;
